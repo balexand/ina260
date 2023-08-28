23c3738c5ebb9f22705a4092eece6d7db7e48e9d
Brian Alexander <balexand@gmail.com>
2023-08-28 16:16:03 -0600

    first commit

---
+++ lib/ina260.ex
@@ -0,0 +1,18 @@
+defmodule Ina260 do
+  @moduledoc """
+  Documentation for `Ina260`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ina260.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ina260_test.exs
@@ -0,0 +1,8 @@
+defmodule Ina260Test do
+  use ExUnit.Case
+  doctest Ina260
+
+  test "greets the world" do
+    assert Ina260.hello() == :world
+  end
+end
